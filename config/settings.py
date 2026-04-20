014387b20dee5ea38f65004fa087ca8a8eed8f43
Adarsh Wahewal <wahewaladarsh8487@gmail.com>
2026-04-20 23:57:45 +0530

    allowed host

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3(7qq_y9arwpvd##e$mo5f%#d+ho9wk!dn*madtesg*q2i3@_^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','localhost','furyawm777.pythonanywhere.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
